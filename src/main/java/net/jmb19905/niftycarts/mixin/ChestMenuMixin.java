6e1a97bf8552774621f3f2f1393b33239f544380
jmb05 <jmb05@web.de>
2026-08-10 14:26:54 +0200

    Cleanup

--- src/main/java/net/jmb19905/niftycarts/mixin/ChestMenuMixin.java
+++ src/main/java/net/jmb19905/niftycarts/mixin/ChestMenuMixin.java
@@ -1,11 +1,9 @@
 package net.jmb19905.niftycarts.mixin;
 
-import net.jmb19905.niftycarts.entity.AbstractCargoCart;
 import net.jmb19905.niftycarts.entity.AbstractDrawnInventoryEntity;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.Container;
 import net.minecraft.world.inventory.ChestMenu;
